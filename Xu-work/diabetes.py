a261c87e7240de4d8f353262102174793a921b21
Wen <64304185+Wind-Storm-X@users.noreply.github.com>
2023-10-02 21:13:24 -0700

    Create diabetes.py

---
+++ Xu-work/diabetes.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn import datasets
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+pima_df = pd.read_csv("diabetes.csv")
+
+pima_df.head()
+
+pima_df.describe()
+
+pima_df.info()
+
+shape = pima_df.shape
+print(shape)
+
+pima_df.isnull().sum()
+
+X = pima_df.drop("Outcome", axis=1)
+
+scaler = StandardScaler()
+X = pd.DataFrame(scaler.fit_transform(X), columns=['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age'])
+
+y = pima_df["Outcome"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1, max_iter = 25000)
+
+clf = Perceptron(tol=0.00001, eta0 = 0.1, max_iter = 25000)
+
+clf.fit(X_train, np.ravel(y_train))
+
+y_pred = clf.predict(X_test)
+
+y_true = y_test
+
+accuracy = metrics.accuracy_score(y_test, y_pred)
+precision = metrics.precision_score(y_test, y_pred, average='weighted')
+recall = metrics.recall_score(y_test, y_pred, average='weighted')
+f1_score = metrics.f1_score(y_test, y_pred, average='weighted')
+
+print("Accuracy:", accuracy)
+print("Precision Score:", precision)
+print("Recall Score: ", recall)
+print("F1 Score: ", f1_score)
